832d2e3f85fece9a835228d866645600d370eced
Brandon Alexis <brandonmachado159@gmail.com>
2022-06-21 17:38:57 -0500

    adicion de restriciones de desplazamiento de carrousel al activar evento de botones

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,21 +1,28 @@
 const displace = 100; // desplazamiento de elemenos
 let count = 0; // contador
-let list_length = $(".carrousel__item").length - 1; // cantidad de elementos en el carrousel
+let list_length = -($(".carrousel__item").length - 1) ; // cantidad de elementos en el carrousel
 
-/*
-
-0 -100 -200 -300
-
-*/
-
+// Evento para boton derecho del carrousel
 $(".carrousel__row--right").click(function () {
-  count += displace;
-  $(".carrousel__list").css("margin-left", `${count}vw`);
-  console.log(count);
+  /**
+   * Validacion que el desplazamiento del condador 
+   * se encuentre en por debajo de 0
+   */
+  if (count < 0) {
+    count += displace;
+    $(".carrousel__list").css("margin-left", `${count}vw`);
+  }
 });
 
+// Evento para boton derecho del carrousel
 $(".carrousel__row--left").click(function () {
-  count -= displace;
-  $(".carrousel__list").css("margin-left", `${count}vw`);
-  console.log(count);
+  /**
+   * Validacion que el desplazamiento del condador 
+   * se encuentre encima del numero de elementos 
+   * por el desplazamiento
+   */
+  if (count > list_length * displace) {
+    count -= displace;
+    $(".carrousel__list").css("margin-left", `${count}vw`);
+  }
 });
