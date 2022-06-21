d3a0c37cafa9ea917730245193b39b1c0191a214
Brandon Alexis <brandonmachado159@gmail.com>
2022-06-20 19:13:34 -0500

    adicion de funcionalidad incompleta de el carrousel(definir limites de traslado)

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,5 +1,21 @@
-let count = 0;
+const displace = 100; // desplazamiento de elemenos
+let count = 0; // contador
+let list_length = $(".carrousel__item").length - 1; // cantidad de elementos en el carrousel
 
-$('.carrousel__row--left').click(function(){
-    
+/*
+
+0 -100 -200 -300
+
+*/
+
+$(".carrousel__row--right").click(function () {
+  count += displace;
+  $(".carrousel__list").css("margin-left", `${count}vw`);
+  console.log(count);
+});
+
+$(".carrousel__row--left").click(function () {
+  count -= displace;
+  $(".carrousel__list").css("margin-left", `${count}vw`);
+  console.log(count);
 });
